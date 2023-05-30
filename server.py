a1d45e5030176ab7ce047d33b5bbf6d2cef45abc
liliroconnell <liliana.oconnell@marsden.school.nz>
2023-05-30 14:48:16 +1200

    Updates to Database

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from db_function import run_search_query_tuples
+from db_function import run_search_query_tuples, run_commit_query
 from datetime import datetime
 
 app = Flask(__name__)
@@ -25,19 +25,80 @@ def halloffame():
 
 @app.route('/training')
 def training():
-    # query for page
-    sql = """ select blog.blog_id, blog.title, blog.content, blog.date, member.name
-    from blog
-    join member on blog.member_id= member.member_id
-    order by blog.date desc;
-    """
-    result = run_search_query_tuples(sql, (), db_path, True)
-    print(result)
-    return render_template("training.html", blog=result)
+    return render_template("training.html")
+
 
 @app.route('/contact')
 def contact():
     return render_template("contact.html")
+
+@app.route('/blog')
+def blog():
+    # query for page
+    sql = """ select blog.blog_id, blog.title, blog.content, blog.date, member.name
+        from blog
+        join member on blog.member_id= member.member_id
+        order by blog.date desc;
+        """
+    result = run_search_query_tuples(sql, (), db_path, True)
+    print(result)
+    return render_template("blog.html", blog=result)
+
+@app.route('/blog_cud')
+def blog_cud():
+    # collect data from the web address
+    data = request.args
+    required_keys = ['id', 'task']
+    for k in required_keys:
+        if k not in data.keys():
+            message = "Do not know what to do with create read update on news (key not present)"
+            return render_template('error.html', message=message)
+    # have an id and a task key
+    if request.method == "GET":
+        if data['task'] == 'delete':
+            sql = "delete from blog where blog_id =?"
+            values_tuple = (data['id'],)
+            result = run_commit_query(sql, values_tuple, db_path)
+            return redirect(url_for('blog'))
+        elif data['task'] == 'update':
+            sql = """ select title, content from blog where blog_id=?"""
+            values_tuple = (data['id'],)
+            result = run_search_query_tuples(sql, values_tuple, db_path, True)
+            result = result[0]
+            return render_template("blog_cud.html",
+                                   **result,
+                                   id=data['id'],
+                                   task=data['task'])
+        elif data['task'] == 'add':
+            temp = {'title':'Test Title', 'content':'Test Content'}
+            return render_template("blog_cud.html",
+                                   id=0,
+                                   task=data['task'],
+                                   **temp)
+        else:
+            message = "Unrecognised task coming from news page"
+            return render_template('error.html', message=message)
+    elif request.method == "POST":
+        # collected form information
+        f = request.form
+        print(f)
+        if data['task'] == 'add':
+            # add the new entry to the database
+            # member is fixed for now
+            sql = """insert into blog(title,content,date,member_id)
+                    values(?,?,?, datetime('now', 'localtime'),2)"""
+            values_tuple = (f['title'], f['content'])
+            result = run_commit_query(sql, values_tuple, db_path)
+            return redirect(url_for('blog'))
+        elif data['task'] == 'update':
+            sql = """update blog set title=?, content=?, date=datetime('now') where blog_id=?"""
+            values_tuple = (f['title'], f['content'], data['id'])
+            result = run_commit_query(sql, values_tuple, db_path)
+            return redirect(url_for('blog'))
+
+    return render_template("blog_cud.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
